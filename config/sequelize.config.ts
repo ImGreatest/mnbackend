936e8ce56064f8e6722ed0835df2308267011af4
ImGreatest <cmayll@mail.ru>
2024-09-03 22:34:03 +0300

    sequelize.config.ts database config

--- config/sequelize.config.ts
+++ config/sequelize.config.ts
@@ -1,23 +1,13 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+import { databaseConfig } from "./database.config";
+
+module.exports = {
+  development: {
+    ...databaseConfig,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    ...databaseConfig,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    ...databaseConfig,
+  },
+};
