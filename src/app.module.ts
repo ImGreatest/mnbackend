85e5f999f53796c7de04240549c55fa761409e02
ImGreatest <cmayll@mail.ru>
2024-09-06 23:17:59 +0300

    off redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from "./controllers/controllers.module";
 import { PrismaModule } from "../libs/services/prisma/prisma.module";
-import { RedisModuleWrapper } from "../libs/services/redis/redis.module";
 
 @Module({
   imports: [
     PrismaModule,
-    RedisModuleWrapper,
     ControllersModule,
   ],
 })
