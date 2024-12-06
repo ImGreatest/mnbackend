a7273c90dd47f6839076896c47173ae4c48b1dc5
ImGreatest <cmayll@mail.ru>
2024-12-07 01:49:18 +0300

    some change struct main.ts and struct launch project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from "./controllers/controllers.module";
 import { PrismaModule } from "../libs/services/prisma/prisma.module";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
 
 @Module({
-  imports: [PrismaModule, ControllersModule],
+  imports: [
+    PrismaModule,
+    ControllersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
