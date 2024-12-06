6de2a9de4be969278b99b73770ea5a458b371e48
ImGreatest <cmayll@mail.ru>
2024-12-07 01:59:04 +0300

    some change struct main.ts and struct launch project

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { logger } from "../logger/logger";
 import { config } from "../config/config";
+import { swagger } from "./swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const docConfig = new DocumentBuilder()
-    .setTitle('MN Cloth Api')
-    .setDescription('Api for mn')
-    .setVersion('1.1.0')
-    .addTag('MN')
-    .build();
-  const doc = SwaggerModule.createDocument(app, docConfig);
-
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-
-  SwaggerModule.setup('api', app, doc);
+  swagger(app);
 
   await app.listen(config.PostgresPort);
 }
