efcb5b30daef44eae09d19a0bb25c55458b3ebe6
ImGreatest <cmayll@mail.ru>
2025-01-03 00:45:15 +0300

    change signature const MockDataReqProduct -> mock-data-req-product.ts

--- libs/domain/product/mocks/const/mock-data-req-product.ts
+++ libs/domain/product/mocks/const/mock-data-req-product.ts
@@ -1,9 +1,10 @@
 import { IProduct } from "@entities";
 import { faker } from "@faker-js/faker";
+import { IExcludeBasicProperties } from "../../../../shared/interfaces/exclude-basic-properties.interface";
 
-export const MockDataReqProduct: Omit<IProduct, "id" | "createdAt" | "updatedAt" | "deletedAt"> = {
+export const MockDataReqProduct: Omit<IProduct, keyof IExcludeBasicProperties> = {
 	name: 'Test name product',
-	description: faker.lorem.text(),
+	desc: faker.lorem.text(),
 	cost: 99999,
 	compound: faker.lorem.text(),
 	collectionId: faker.string.uuid(),
