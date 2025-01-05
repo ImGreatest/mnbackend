ecbe1f72d0425a93c6857f014cf6e68a60bf7ac5
ImGreatest <cmayll@mail.ru>
2025-01-05 13:49:50 +0300

    fix way to user.repository.ts

--- libs/domain/user/user.module.ts
+++ libs/domain/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { CryptoModule } from "../../services/crypto/crypto.module";
 import { UserService } from "./user.service";
-import { UserRepository } from "./repositories/user.repository";
+import { UserRepository } from "./repository/user.repository";
 import { UserAdapter } from "../../adapter/user/user.adapter";
 
 @Module({
