dcf213b8c657149b5f1703ec8ff1dfb9edd13158
ImGreatest <ImGreatest@users.noreply.github.com>
2025-01-16 16:33:35 +0300

    adds property about category

--- libs/domain/product/dto/req-dto/req-create-product.dto.ts
+++ libs/domain/product/dto/req-dto/req-create-product.dto.ts
@@ -11,7 +11,7 @@ export class ReqCreateProductDto implements Omit<IProduct, keyof IExcludeBasicPr
 		description: 'Namely of product',
 		example: faker.commerce.product(),
 	})
-	@IsString()
+	@IsString({ message: "Name value type must be string" })
 	name: string;
 
 	@ApiProperty({
@@ -20,7 +20,7 @@ export class ReqCreateProductDto implements Omit<IProduct, keyof IExcludeBasicPr
 		description: 'Property of description product',
 		example: faker.lorem.sentence(),
 	})
-	@IsString()
+	@IsString({ message: "Description value type must be string" })
 	desc?: string;
 
 	@ApiProperty({
@@ -38,15 +38,24 @@ export class ReqCreateProductDto implements Omit<IProduct, keyof IExcludeBasicPr
 		description: 'Description product compound',
 		example: faker.lorem.slug(),
 	})
-	@IsString()
+	@IsString({ message: "Compound value type must be string" })
 	compound: string;
 
 	@ApiProperty({
 		type: String,
 		required: true,
-		description: 'Property for identifier collection to refer product',
+		description: 'Property identifier collection to refer product',
 		example: faker.string.uuid(),
 	})
-	@IsString()
+	@IsString({ message: "Collection identifier value type must be string" })
 	collectionId: string;
+
+	@ApiProperty({
+		type: String,
+		required: true,
+		description: "Property identifier category to refer product",
+		example: faker.string.uuid(),
+	})
+	@IsString({ message: "Category identifier value must be string" })
+	categoryId: string;
 }
