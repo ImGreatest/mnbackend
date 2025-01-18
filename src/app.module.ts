caac347321d51d636c6f70e1e946749cb6825006
ImGreatest <ImGreatest@users.noreply.github.com>
2025-01-18 06:59:18 +0300

    adds https

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { ControllersModule } from "./controllers/controllers.module";
 import { PrismaModule } from "../libs/services/prisma/prisma.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "./services/auth/auth.module";
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PrismaModule,
-    AuthModule,
     ControllersModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from "./app.module";
 import { logger } from "../logger/logger";
 import { config } from "../config/config";
 import { swagger } from "./swagger";
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('./cert.key'),
+      cert: fs.readFileSync('./cert.crt'),
+    },
+  });
 
   app.setGlobalPrefix("api");
   app.enableCors({
@@ -19,7 +25,7 @@ async function bootstrap() {
   await app.listen(config.PostgresPort);
 }
 bootstrap().then((r) => {
-    const url = `http://${config.DatabaseHost}:${config.ApiPort}/api`;
+    const url = `https://${config.DatabaseHost}:${config.ApiPort}/api`;
     const link = `\u001b]8;;${url}\u001b\\Api way ${url}\u001b]8;;\u001b\\`;
     logger.info(link);
 });
